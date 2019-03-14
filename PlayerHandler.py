da34e7d27dc5c6360ab25eef2cd45b78a9614326
Callum <hazedcube@gmail.com>
2019-03-14 23:43:26 +0000

    Minor refactoring, working on enemies as a group/fleet

---
+++ PlayerHandler.py
@@ -0,0 +1,81 @@
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+from VectorHandler import Vector
+
+spaceship = simplegui.load_image("https://imgur.com/gudzU5Z.png")
+
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+PLAYER_SPEED = 0.9
+PLAYER_IMG_SIZE = (100, 100)
+PLAYER_IMG_CENTER = (50, 50)
+PLAYER_IMG_ROTATION = 0
+
+class Player:
+    def __init__(self, pos):
+        self.pos = pos
+        self.vel = Vector()
+        self.radius = 30
+
+    def draw(self, canvas):
+        #canvas.draw_circle(self.pos.getP(), self.radius, 1, 'White', 'White')
+        canvas.draw_image(spaceship, PLAYER_IMG_CENTER, PLAYER_IMG_SIZE , self.pos.getP(), PLAYER_IMG_SIZE , PLAYER_IMG_ROTATION)
+
+    def move(self):
+        self.pos.add(self.vel)
+        self.vel.multiply(PLAYER_SPEED)
+        if self.pos == Vector(0, WIN_WIDTH):
+            self.stop()
+        if not self.isStationary() and self.vel.length() < 0.1:
+            self.stop()
+
+    def stop(self):
+        self.vel = Vector()
+
+    def isStationary(self):
+        return self.vel.length() == 0
+
+    def hitL(self):
+        return self.pos.x - self.radius
+
+    def hitR(self):
+        return self.pos.x + self.radius
+
+    def animate(self, canvas):
+        self.move()
+        self.draw(canvas)
+
+class KB:
+    def __init__(self):
+        self.player = player
+        self.left = False
+        self.right = False
+
+    def keydown(self, key):
+        if key == simplegui.KEY_MAP["left"]:
+            self.left = True
+        elif key == simplegui.KEY_MAP["right"]:
+            self.right = True
+        #if key == simplegui.KEY_MAP["p"]:
+        #    timer.stop()
+        #    frame.stop()
+
+
+    def keyup(self, key):
+        if key == simplegui.KEY_MAP["left"]:
+            self.left = False
+        elif key == simplegui.KEY_MAP["right"]:
+            self.right = False
+
+    def update(self):
+        if self.left:
+            self.player.vel.subtract(Vector(PLAYER_SPEED, 0))
+        if self.right:
+            self.player.vel.add(Vector(PLAYER_SPEED, 0))
+
+
+player = Player(Vector(WIN_WIDTH/2, WIN_HEIGHT/1.1))
+keyboard = KB()
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+'''
+Main class for both single Enemies and Enemies as a group. Includes a player sprite
+(with some basic left-right movement) and keyboard class for testing purposes.
+
+Press 'P' to properly exit the game, as currently SimpleGUI likes to inform you that
+there is still a timer running when you exit, which I need to fix.
+
+TO-DO:
+- Sort what from Enemy class is needed in Enemies class
+- Further add to Enemies class (ability to be hit individually, ability to shoot bullets etc.)
+- General code clean-up (probably in accordance with PEP 8
+'''
+
 try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
-from VectorHandler import Vector
-#from Keyboard import KB
+from VectorHandler import *
+from PlayerHandler import *
 import time
 
 # CONSTANTS
@@ -14,52 +27,16 @@ PLAYER_SPEED = 0.9
 ENEMY_SPEED = 10
 counter = 0
 
-pi_size = (100, 100)
-pi_center = (50, 50)
-pi_rotation = 0
+ENEMY_IMG_SIZE = (48, 48)
+ENEMY_IMG_CENTER = (24, 24)
+ENEMY_IMG_ROTATION = 0
 
-ei_size = (48, 48)
-ei_center = (24, 24)
-ei_rotation = 0
 e_x = 85
+e_start_x = 260
+e_start_y = 70
+e_fleet_gap = 50
 
-spaceship = simplegui.load_image("https://imgur.com/gudzU5Z.png")
 enemy = simplegui.load_image("https://imgur.com/fI8cyfM.png")
-
-class Player:
-    def __init__(self, pos):
-        self.pos = pos
-        self.vel = Vector()
-        self.radius = 30
-
-    def draw(self, canvas):
-        #canvas.draw_circle(self.pos.getP(), self.radius, 1, 'White', 'White')
-        canvas.draw_image(spaceship, pi_center, pi_size, self.pos.getP(), pi_size, pi_rotation)
-
-    def move(self):
-        self.pos.add(self.vel)
-        self.vel.multiply(PLAYER_SPEED)
-        if self.pos == Vector(0, WIN_WIDTH):
-            self.stop()
-        if not self.isStationary() and self.vel.length() < 0.1:
-            self.stop()
-
-    def stop(self):
-        self.vel = Vector()
-
-    def isStationary(self):
-        return self.vel.length() == 0
-
-    def hitL(self):
-        return self.pos.x - self.radius
-
-    def hitR(self):
-        return self.pos.x + self.radius
-
-    def animate(self, canvas):
-        self.move()
-        self.draw(canvas)
-
 
 class Enemy:
     def __init__(self, pos):
@@ -71,7 +48,7 @@ class Enemy:
 
     def draw(self, canvas):
         #canvas.draw_circle(self.pos.getP(), self.radius, 1, 'white', 'white')
-        canvas.draw_image(enemy, ei_center, ei_size, self.pos.getP(), ei_size, ei_rotation)
+        canvas.draw_image(enemy, ENEMY_IMG_CENTER, ENEMY_IMG_SIZE, self.pos.getP(), ENEMY_IMG_SIZE, ENEMY_IMG_ROTATION)
 
     def move(self):
         if self.mRight:
@@ -95,79 +72,56 @@ class Enemy:
         self.draw(canvas)
         self.move()
 
-
-class KB:
-    def __init__(self):
-        self.player = player
-        self.left = False
-        self.right = False
-
-    def keydown(self, key):
-        if key == simplegui.KEY_MAP["left"]:
-            self.left = True
-        elif key == simplegui.KEY_MAP["right"]:
-            self.right = True
-        if key == simplegui.KEY_MAP["p"]:
-            timer.stop()
-            frame.stop()
+class Enemies:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
 
 
-    def keyup(self, key):
-        if key == simplegui.KEY_MAP["left"]:
-            self.left = False
-        elif key == simplegui.KEY_MAP["right"]:
-            self.right = False
 
-    def update(self):
-        if self.left:
-            self.player.vel.subtract(Vector(PLAYER_SPEED, 0))
-        if self.right:
-            self.player.vel.add(Vector(PLAYER_SPEED, 0))
-
-
-player = Player(Vector(WIN_WIDTH/2, WIN_HEIGHT/1.1))
-keyboard = KB()
-enemies = []
+eList = []
 x = e_x
 moved = 0
 t = 0
 
-for n in range(6):
+def make_enemies():
+    for row in range(3):
+        for col in range(6):
+            posV = Vector(e_start_x + (col * e_fleet_gap), e_start_y + (row * e_fleet_gap))
+            en = Enemy(posV)
+            eList.append(en)
+
+
+'''for n in range(6):
     x = x + 50
     y = 100
     posi = Vector(x,y)
     e = Enemy(posi)
-    enemies.append(e)
-
-x = e_x
-for n in range(6):
-    x = x + 50
-    y = 150
-    posi = Vector(x, y)
-    e = Enemy(posi)
-    enemies.append(e)
+    enemies.append(e)'''
 
 def move_objects():
     global counter, t
 
-    counter +=1
+    counter += 1
 
     #make enemies move faster
     if counter % ENEMY_SPEED == 0:
-        for enemy in enemies:
+        for enemy in eList:
             enemy.move()
             #if enemy.pos.x > 700:
              #   enemy.moveDown()
             if counter % (ENEMY_SPEED * 15) == 0:
                 enemy.moveDown()
 
-
+#print(enemies)
 def draw(canvas):
     player.animate(canvas)
     keyboard.update()
-    for enemy in enemies:
+    for enemy in eList:
         enemy.draw(canvas)
         #enemy.move()
+
+make_enemies()
 
 # Frame creation
 frame = simplegui.create_frame("Main", WIN_WIDTH, WIN_HEIGHT)
