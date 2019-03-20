2e4c14f414c16061ab6d3d144d804069effa83e5
11laurenm <46924967+11laurenm@users.noreply.github.com>
2019-03-20 22:44:02 +0000

    Update Interactions.py

--- Interactions.py
+++ Interactions.py
@@ -1,36 +1,40 @@
 class Interaction:
 
     def __init__(self, EBULLETS, BULLETS, playerOne, playerTwo, eList):
-        self.EBULLETS = EBULLETS
+        self.E_BULLETS = EBULLETS
         self.BULLETS = BULLETS
         self.playerOne = playerOne
         self.playerTwo = playerTwo
-        self.Enemies = eList
+        self.eList = eList
 
-        def update(self):
-            for bullet in BULLETS():
-                for enemy in eList:
+    def update(self):
+            for bullet in BULLETS:
+                for enemy in self.eList:
                     if bullet.pos == enemy.pos:
-                        eList.remove(enemy)  # remove or lower health?
-                        EBULLETS.remove(bullet)
+                        self.eList.remove(enemy)  # remove or lower health?
+                        BULLETS.remove(bullet)
                         #increase score
                         KILLED = KILLED + 1 ###
 
-            for bullet in EBULLETS():
-                if bullet.pos == playerOne.pos:
-                    playerOne.LIVES = playerOne.LIVES - 1
-                    BULLETS.remove(bullet)
-                elif bullet.pos == playerTwo.pos:
-                    playerTwo.LIVES = playerTwo.LIVES - 1
-                    BULLETS.remove(bullet)
+            for bullet in E_BULLETS:
+                if bullet.pos.y > playerOne.pos.y - 60:
+                    if bullet.pos.x > playerOne.pos.x and bullet.pos.x < playerOne.pos.x + 75:
+                        if playerOne.LIVES > 0:
+                            playerOne.LIVES = playerOne.LIVES - 1
+                        E_BULLETS.remove(bullet)
+                    elif bullet.pos.x > playerTwo.pos.x and bullet.pos.x < playerTwo.pos.x + 75:
+                        if playerTwo.LIVES > 0:
+                            playerTwo.LIVES = playerTwo.LIVES - 1
+                        E_BULLETS.remove(bullet)
 
-                if playerOne.LIVES == 0 and playerTwo.LIVES == 0:
-                    # Game over screen
-                    pass
-                elif playerOne.LIVES == 0:
-                    # Disable playerOne
-                    pass
-                elif playerTwo.LIVES == 0:
-                    # Disable playerTwo
-                    pass
-
+                    if playerOne.LIVES == 0 and playerTwo.LIVES == 0:
+                        # Game over screen
+                        sys.exit('Both players ran out of lives')
+                    elif playerOne.LIVES == 0:
+                        # Disable playerOne
+                        pass
+                    elif playerTwo.LIVES == 0:
+                        # Disable playerTwo
+                        pass
+                print('Player One lives: ' + str(playerOne.LIVES))
+                print('Player Two lives: ' + str(playerTwo.LIVES))
