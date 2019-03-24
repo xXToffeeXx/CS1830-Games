53b21f0f85689ae79f88764da6ebfc7f00df95de
11laurenm <46924967+11laurenm@users.noreply.github.com>
2019-03-24 23:08:41 +0000

    Update game.py

--- game.py
+++ game.py
@@ -5,6 +5,109 @@ except ImportError:
 
 import math, random, sys, time
 
+# The Vector class
+class Vector:
+
+    # Initialiser
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    # Returns a string representation of the vector
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+    # Tests the equality of this vector and another
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    # Tests the inequality of this vector and another
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+        # Returns a tuple with the point corresponding to the vector
+
+    def getP(self):
+        return (self.x, self.y)
+
+    # Returns a copy of the vector
+    def copy(self):
+        return Vector(self.x, self.y)
+
+    # Adds another vector to this vector
+    def add(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __add__(self, other):
+        return self.copy().add(other);
+
+    # Negates the vector (makes it point in the opposite direction)
+    def negate(self):
+        return self.multiply(-1)
+
+    def __neg__(self):
+        return self.copy().negate()
+
+    # Subtracts another vector from this vector
+    def subtract(self, other):
+        return self.add(-other)
+
+    def __sub__(self, other):
+        return self.copy().subtract(other)
+
+    # Multiplies the vector by a scalar
+    def multiply(self, k):
+        self.x *= k
+        self.y *= k
+        return self
+
+    def __mul__(self, k):
+        return self.copy().multiply(k)
+
+    def __rmul__(self, k):
+        return self.copy().multiply(k)
+
+    # Divides the vector by a scalar
+    def divide(self, k):
+        return self.multiply(1 / k)
+
+    def __truediv__(self, k):
+        return self.copy().divide(k)
+
+    # Normalizes the vector
+    def normalize(self):
+        return self.divide(self.length())
+
+    # Returns a normalized version of the vector
+    def getNormalized(self):
+        return self.copy().normalize()
+
+    # Returns the dot product of this vector with another one
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+
+    # Returns the length of the vector
+    def length(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    # Returns the squared length of the vector
+    def lengthSquared(self):
+        return self.x ** 2 + self.y ** 2
+
+    # Reflect this vector on a normal
+    def reflect(self, normal):
+        n = normal.copy()
+        n.multiply(2 * self.dot(normal))
+        self.subtract(n)
+        return self
+
+    # Returns the angle between this vector and another one
+    # You will need to use the arccosine function:
+    # acos in the math library
+    def angle(self, other):
+        return math.acos(self.dot(other) / (self.length() * other.length()))
 
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 600
@@ -488,14 +591,12 @@ def game_over(canvas, cond):
     if gameover:
         cond = 'lose'
     if cond == 'win':
-        canvas.draw_text("You Win!", (CANVAS_WIDTH / 2.75, CANVAS_HEIGHT / 2), 50, 'White', 'sans-serif')
+        canvas.draw_text("Level " + str(LEVEL) + " cleared!", (CANVAS_WIDTH / 2.75, CANVAS_HEIGHT / 2), 50, 'White', 'sans-serif')
         canvas.draw_text("Score: " + str(KILLED), (CANVAS_WIDTH / 2.55, CANVAS_HEIGHT / 1.75), 40, 'White',
                          'sans-serif')
-        time.sleep(10)
-        set_up()
-        '''if incount >= 200:
-            sys.exit()
-        incount += 1 '''
+        if incount >= 200:
+            set_up()
+        incount += 1
     if cond == 'lose':
         canvas.draw_polygon([[0, 0], [0, CANVAS_HEIGHT], [CANVAS_WIDTH, CANVAS_HEIGHT], [CANVAS_WIDTH, 0]], 12,
                             'Black', 'Black')
@@ -506,8 +607,6 @@ def game_over(canvas, cond):
             sys.exit()
         incount += 1
 
-
-incount = 0
 
 
 def set_up():
@@ -520,6 +619,7 @@ def reset():
     global E_ROWS
     global E_COLS
     global ENEMY_SPEED
+    global incount
     playerOne.LIVES = 3
     playerTwo.LIVES = 3
     playerOne.disable = False
@@ -530,6 +630,7 @@ def reset():
     E_ROWS = 4
     E_COLS = 4 + LEVEL
     ENEMY_SPEED = 50 + (LEVEL * 5)
+    incount = 0
 
 
 inter = Interaction(E_BULLETS, BULLETS, playerOne, playerTwo, ENEMIES, WALLS)
@@ -541,3 +642,4 @@ frame.set_draw_handler(draw)
 frame.set_keydown_handler(controls.keyDown)
 frame.set_keyup_handler(controls.keyUp)
 frame.start()
+  
